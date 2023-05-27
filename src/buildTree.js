9f9b665fb192a0bd02007a03777db9f9088c6eb5
Madorskii <madorskiy.017@yandex.ru>
2023-05-27 16:43:51 -0300

    update buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -5,10 +5,8 @@ import _ from 'lodash'
 const buildTree = (filePath1, filePath2) => {
     const difference = []
     let difString = ''
-    const [file1, type1] = getFile(filePath1)
-    const [file2, type2] = getFile(filePath2)
-    const data1 = parser(file1, type1)
-    const data2 = parser(file2, type2)
+    const data1 = parser(...getFile(filePath1))
+    const data2 = parser(...getFile(filePath2))
     const keys = Array.from(
         new Set([...Object.keys(data1), ...Object.keys(data2)].sort())
     )
