58bfbb5507857c33a504929a32a8ce5199a7f6c5
Madorskii <madorskiy.017@yandex.ru>
2023-05-27 19:36:52 -0300

    tests updated

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-undef */
-import resultFlat from '../_fixtures_/resultFlatFile.js'
+import resultFlat from '../_fixtures_/flat/resultFlatFile.js'
 import genDiff from '../src/genDiff.js'
 
 const jsonFiles = {
-    filePath1: '_fixtures_/flatFile1.json',
-    filePath2: '_fixtures_/flatFile2.json',
+    filePath1: '_fixtures_/flat/flatFile1.json',
+    filePath2: '_fixtures_/flat/flatFile2.json',
 }
 const yamlFiles = {
-    filePath1: '_fixtures_/flatYaml1.yaml',
-    filePath2: '_fixtures_/flatYaml2.yaml',
+    filePath1: '_fixtures_/flat/flatYaml1.yaml',
+    filePath2: '_fixtures_/flat/flatYaml2.yaml',
 }
 test('genDiff should return difference betweet two files depends on extension', () => {
     expect(genDiff(jsonFiles.filePath1, jsonFiles.filePath2)).toEqual(
