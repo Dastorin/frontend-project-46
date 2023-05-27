951f553a1d0fa388eb0142f27a6b8209f2a5f167
Madorskii <madorskiy.017@yandex.ru>
2023-05-27 19:25:11 -0300

    tests were updated

--- _fixtures_/resultFlatFile.js
+++ _fixtures_/resultFlatFile.js
@@ -1,4 +1,2 @@
-const resultFlatJson = `{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}`
-export default resultFlatJson
-
-console.log(resultFlatJson)
+const resultFlat = `{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}`
+export default resultFlat
--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-undef */
 import getFile from '../src/utils/getFile.js'
-import resultFlatJson from '../_fixtures_/resultFlatFile.js'
+import resultFlat from '../_fixtures_/resultFlatFile.js'
 import parser from '../src/parser.js'
 import genDiff from '../src/genDiff.js'
 
-const filePath1 = '_fixtures_/flatFile1.json'
-const filePath2 = '_fixtures_/flatFile2.json'
-const [file1, type1] = getFile(filePath1)
-
-test('genDiff should return difference betweet two files', () => {
-    expect(genDiff(filePath1, filePath2)).toEqual(resultFlatJson)
-})
-
-test('parser should return file following correct extension', () => {
-    expect(parser(file1, type1)).toEqual({
-        host: 'hexlet.io',
-        timeout: 50,
-        proxy: '123.234.53.22',
-        follow: false,
-    })
+const jsonFiles = {
+    filePath1: '_fixtures_/flatFile1.json',
+    filePath2: '_fixtures_/flatFile2.json',
+}
+const yamlFiles = {
+    filePath1: '_fixtures_/flatYaml1.yaml',
+    filePath2: '_fixtures_/flatYaml2.yaml',
+}
+test('genDiff should return difference betweet two files depends on extension', () => {
+    expect(genDiff(jsonFiles.filePath1, jsonFiles.filePath2)).toEqual(
+        resultFlat
+    )
+    expect(genDiff(yamlFiles.filePath1, yamlFiles.filePath2)).toEqual(
+        resultFlat
+    )
 })
