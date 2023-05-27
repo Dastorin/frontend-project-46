d3c7ea4fa654a245d99785288cfc66572240a3ce
Madorskii <madorskiy.017@yandex.ru>
2023-05-27 18:13:10 -0300

    logick updat to buildTree

--- src/buildTree.js
+++ src/buildTree.js
@@ -2,9 +2,7 @@ import _ from 'lodash'
 
 const buildTree = (data1, data2) => {
     const difference = []
-    const keys = Array.from(
-        new Set([...Object.keys(data1), ...Object.keys(data2)].sort())
-    )
+    const keys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]).sort()
     for (const key of keys) {
         if (_.has(data1, `${key}`) && _.has(data2, `${key}`)) {
             if (data1[key] === data2[key]) {
