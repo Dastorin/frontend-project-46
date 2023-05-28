4256a8991919e40117456d66c613f16d750e7545
Madorskii <madorskiy.017@yandex.ru>
2023-05-27 22:47:36 -0300

    buildTree has been adapted to Non-flat files

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,28 +1,33 @@
 import _ from 'lodash'
 
 const buildTree = (data1, data2) => {
-    const difference = []
-    const keys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]).sort()
-    for (const key of keys) {
-        if (_.has(data1, `${key}`) && _.has(data2, `${key}`)) {
-            if (data1[key] === data2[key]) {
-                const difString = `   ${key}: ${data2[key]}`
-                difference.push(difString)
+    const keys = _.union(Object.keys(data1), Object.keys(data2)).sort()
+    const difference = keys
+        .map((key) => {
+            if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+                return `   ${key}: ${buildTree(data1[key], data2[key])}`
             }
+            let str = ''
             if (data1[key] !== data2[key]) {
-                const difString = ` - ${key}: ${data1[key]}\n + ${key}: ${data2[key]}`
-                difference.push(difString)
+                if (_.has(data1, key)) {
+                    str += _.isObject(data1[key])
+                        ? ` - ${key}: ${JSON.stringify(data1[key], null, 4)}`
+                        : ` - ${key}: ${data1[key]}`
+                }
+                if (_.has(data2, key)) {
+                    if (str.length > 0) str += '\n'
+                    str += _.isObject(data2[key])
+                        ? ` + ${key}: ${JSON.stringify(data2[key], null, 4)}`
+                        : ` + ${key}: ${data2[key]}`
+                }
             }
-        }
-        if (_.has(data1, `${key}`) && !_.has(data2, `${key}`)) {
-            const difString = ` - ${key}: ${data1[key]}`
-            difference.push(difString)
-        }
-        if (!_.has(data1, `${key}`) && _.has(data2, `${key}`)) {
-            const difString = ` + ${key}: ${data2[key]}`
-            difference.push(difString)
-        }
-    }
-    return ['{\n', difference.join('\n'), '\n}'].join('')
+            if (data1[key] === data2[key]) {
+                str += `   ${key}: ${data1[key]}`
+            }
+            return str
+        })
+        .join('\n')
+    return `{\n${difference}\n}`
 }
+
 export default buildTree
