b47949965558dfe89fa1bdd1caa4e31f0d5876bd
Madorskii <madorskiy.017@yandex.ru>
2023-05-29 00:20:02 -0300

    stylish upd v2

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,22 +4,22 @@ const stylish = (tree) => {
     const iter = (node, depth = 1) => {
         const indent = ' '.repeat(depth * 2)
         const line = node.map((children) => {
-            const { type } = children
+            const type = children.type
             switch (type) {
                 case 'nested': {
-                    return `${indent} ${children}: ${iter(node, (depth += 1))}`
+                    return `${indent} ${children.key}: ${iter(children.children, (depth += 1))}`
                 }
                 case 'changed': {
-                    return `${indent} - ${children}: ${node.value1}\n${indent} + ${children}: ${node.value2}`
+                    return `${indent} - ${children.key}: ${children.value1}\n${indent} + ${children.key}: ${children.value2}`
                 }
                 case 'unchanged': {
-                    return `${indent} ${children}: ${node.value}`
+                    return `${indent} ${children.key}: ${children.value}`
                 }
                 case 'deleted': {
-                    return `${indent} - ${children}: ${node.value}`
+                    return `${indent} - ${children.key}: ${children.value}`
                 }
                 case 'added': {
-                    return `${indent} + ${children}: ${node.value}`
+                    return `${indent} + ${children.key}: ${children.value}`
                 }
             }
         })
